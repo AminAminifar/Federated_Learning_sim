d03e82649a0568b7aab2fea26a290de732eef363
Amin Aminifar <43568226+AminAminifar@users.noreply.github.com>
2022-09-03 18:18:36 +0200

    Add files via upload

---
+++ FL_LocalUpdate/data_holder_party_class.py
@@ -0,0 +1,87 @@
+# imports
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental import preprocessing
+import tensorflow as tf
+
+
+class Party:
+
+    def __init__(self, data, data_labels, tf_seed, num_local_updates):
+        self.data = data
+        self.data_labels = data_labels
+        # Instantiate a loss function.
+        self.loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        self.model = self.define_model()
+        self.grads = None
+        self.num_local_updates = num_local_updates
+        self.tf_seed = tf_seed
+
+
+    def define_model(self):
+        """ This function generates the NN model"""
+
+        model = keras.Sequential([
+            layers.InputLayer(input_shape=[28, 28]),
+
+            #             # Data Augmentation
+            #             preprocessing.RandomFlip('horizontal'),
+            #             preprocessing.RandomContrast(0.5),
+
+            #             # Conv
+            #             layers.BatchNormalization(renorm=True),
+            #             layers.Conv2D(filters=64, kernel_size=3, activation='relu', padding='same'),
+            #             layers.MaxPool2D(),
+
+            # Head
+            layers.BatchNormalization(renorm=True),
+            layers.Flatten(),
+            layers.Dense(128, activation='relu'),
+            layers.Dense(10),
+
+        ])
+
+        model.compile(
+            optimizer='adam',
+            loss=self.loss_fn,  # 'binary_crossentropy', categorical_crossentropy
+            metrics=['accuracy'],
+        )
+        return model
+
+    def calculate_gradients(self, x, y):
+        """ This function calculates gradients for one round of feedforward and back propagation """
+
+        with tf.GradientTape() as tape:
+            logits = self.model(x, training=True)
+            loss_value = self.loss_fn(y, logits)
+        self.grads = tape.gradient(loss_value, self.model.trainable_weights)
+
+    def locally_update_model(self):
+        """ This function updates the model one to several times based on local data
+         and returns the updated parameters """
+
+        for i in range(0, self.num_local_updates):
+            # calculate gradients
+            self.calculate_gradients(self.data, self.data_labels)
+            # update model based on calculated grads
+            self.model.optimizer.apply_gradients(zip(self.grads, self.model.trainable_weights))
+
+        # return locally updated model parameters
+        return self.model.get_weights()
+
+    def interface_pipeline(self, global_model_parameters=None):
+        """ This can be called by server/interface """
+
+        if global_model_parameters is not None:
+            #  receive global model parameters
+            # update local model by global_model_parameters
+            self.model.set_weights(global_model_parameters)
+        else:
+            tf.random.set_seed(self.tf_seed)
+        # locally update the model
+        # share locally updated model parameters
+        return self.locally_update_model()
+
+
+
+
---
+++ FL_LocalUpdate/generate_parties.py
@@ -0,0 +1,58 @@
+# imports
+import gzip
+import numpy as np
+from pathlib import Path
+from sklearn.utils import shuffle
+import data_holder_party_class
+
+
+# load data
+data_dir = Path('C:/Amin/Workspace/Data/fashion mnist')
+
+# load train data
+f = gzip.open(data_dir/'train-images-idx3-ubyte.gz','r')
+image_size = 28
+num_images = 60000
+
+
+f.read(16)
+buf = f.read(image_size * image_size * num_images)
+train_data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
+train_data = train_data.reshape(num_images, image_size, image_size, 1)
+
+# image = np.asarray(train_data[2]).squeeze()
+# plt.imshow(image, cmap="Greys")
+# plt.show()
+
+# labels
+f = gzip.open(data_dir/'train-labels-idx1-ubyte.gz','r')
+f.read(8)
+
+buf = f.read(num_images)
+train_labels = np.frombuffer(buf, dtype=np.uint8).astype(np.int64)
+# print(labels)
+
+# data information
+num_train_records = 60000
+train_data_record_indices = range(0, num_train_records)
+train_data_record_indices_shuffled = shuffle(train_data_record_indices, random_state=0)
+
+
+def generate_parties(num_data_holder_parties, tf_seed, num_local_updates):
+    """ This function generates data holder parties and return a list of generated parties"""
+
+    # indices of training data samples for each data holder party
+    chunk_indices = np.array_split(train_data_record_indices_shuffled, num_data_holder_parties)
+
+    parties = []
+    for i in range(num_data_holder_parties):
+        parties.append(data_holder_party_class.Party(data=train_data[chunk_indices[i]],
+                                                     data_labels=train_labels[chunk_indices[i]],
+                                                     tf_seed=tf_seed,
+                                                     num_local_updates=num_local_updates))
+
+    print("A list of parties have been created.\n\
+    Data samples randomly assigned to each party.\n\
+    The number of samples for each party is almost the same.")
+
+    return parties
